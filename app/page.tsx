f33185b36de9374129af730900c9694434de5145
quentin72000 <quentin72000@yahoo.com>
2024-08-05 19:06:36 +0200

    chore: Add console logs for debugging purposes

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,9 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function Home() {
   console.info("Metadata", await generateMetadata());
+  console.log("App URL", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000");
+  console.log("Current URL", new URL("/frames", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"));
   console.info("Frame metadata", await fetchMetadata(new URL("/frames", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")));
+  console.info("Test frame metadata", await fetchMetadata(new URL("/", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")));
   return <div>GM user data example.</div>;
 }
