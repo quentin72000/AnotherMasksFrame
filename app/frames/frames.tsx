a668cadf974f44afc039afd27cf41ef6bc488835
quentin72000 <quentin72000@yahoo.com>
2024-08-17 23:34:57 +0200

    Revert Disable default image worker route

--- app/frames/frames.tsx
+++ app/frames/frames.tsx
@@ -12,7 +12,6 @@ import { PiDeviceRotate } from "react-icons/pi";
 
 export const frames = createFrames({
   basePath: '/frames',
-  imagesRoute: null, // Disable default image worker route to mitigate 'URI Too Long" error
   baseUrl: appURL(),
   middleware: [
     farcasterHubContext({
