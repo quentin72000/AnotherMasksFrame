859b633f62df751ead1dd49c42d3462a43a7fd97
quentin72000 <quentin72000@yahoo.com>
2024-08-30 18:19:31 +0200

    Update package dependencies and add metadata caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { fetchMetadata } from "frames.js/next";
 import { Metadata } from "next";
 
+
+let metadata: any = null;
 export async function generateMetadata(): Promise<Metadata> {
+  if (!metadata) {
+    metadata = await fetchMetadata(
+      new URL("/frames", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
+    );
+    console.log("Metadata cached !");
+  }
   return {
     title: "$Masks Stats by quentin72000",
     other: {
-      ...(await fetchMetadata(
-        new URL("/frames", process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000")
-      )),
+      ...metadata,
     },
   };
 }
